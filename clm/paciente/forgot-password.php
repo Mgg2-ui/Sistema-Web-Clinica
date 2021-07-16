4fd9dbb4c6e76733c2f0c002ad35f4b8add4561e
Sapito Sucio <davidselemg@gmail.com>
2021-07-15 22:38:05 -0500

    Locación detallada

--- clm/paciente/forgot-password.php
+++ clm/paciente/forgot-password.php
@@ -1,44 +1,30 @@
 <?php
 session_start();
-error_reporting(0);
+//error_reporting(0);
 include("include/config.php");
-if(isset($_POST['submit']))
+// Code for updating Password
+if(isset($_POST['change']))
 {
-$ret=mysqli_query($con,"SELECT * FROM admin WHERE username='".$_POST['username']."' and password='".$_POST['password']."'");
-$num=mysqli_fetch_array($ret);
-if($num>0)
-{
-$extra="dashboard.php";//
-$_SESSION['login']=$_POST['username'];
-$_SESSION['id']=$num['id'];
-$host=$_SERVER['HTTP_HOST'];
-$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-header("location:http://$host$uri/clm/admin/$extra");
-exit();
+$name=$_SESSION['name'];
+$email=$_SESSION['email'];
+$newpassword=md5($_POST['password']);
+$query=mysqli_query($con,"update users set password='$newpassword' where fullName='$name' and email='$email'");
+if ($query) {
+echo "<script>alert('Contrasena actualizada correctamente.');</script>";
+echo "<script>window.location.href ='user-login.php'</script>";
 }
-else
-{
-$_SESSION['errmsg']="usuario o contrasena invalido";
-$extra="index.php";
-$host  = $_SERVER['HTTP_HOST'];
-$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-header("location:http://$host$uri/$extra");
-exit();
+
 }
-}
+
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Administrador-Login</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
-		<meta content="" name="description" />
-		<meta content="" name="author" />
+		<title>Restablecimiento de contrasena</title>
+		
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="admin_lite/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="admin_lite/fontawesome/css/font-awesome.min.css">
@@ -49,46 +35,69 @@ exit();
 		<link rel="stylesheet" href="assets/css/styles.css">
 		<link rel="stylesheet" href="assets/css/plugins.css">
 		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+
+				<script type="text/javascript">
+function valid()
+{
+ if(document.passwordreset.password.value!= document.passwordreset.password_again.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.passwordreset.password_again.focus();
+return false;
+}
+return true;
+}
+</script>
 	</head>
 	<body class="login">
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<h2>Administrador</h2>
+				<a href="../index.html"><h2> CLM | Restablecimiento de contrasena del paciente</h2></a>
 				</div>
 
 				<div class="box-login">
-					<form class="form-login" method="post">
+					<form class="form-login" name="passwordreset" method="post" onSubmit="return valid();">
 						<fieldset>
 							<legend>
-							Iniciar sesión en su cuenta
+							Restablecimiento de contrasena del paciente
 							</legend>
 							<p>
-							Ingrese su nombre y contraseña para iniciar sesión.<br />
-								<span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
+							Establezca su nueva contrasena.<br />
+								<span style="color:red;"><?php echo $_SESSION['errmsg']; ?><?php echo $_SESSION['errmsg']="";?></span>
 							</p>
-							<div class="form-group">
-								<span class="input-icon">
-									<input type="text" class="form-control" name="username" placeholder="Username">
-									<i class="fa fa-user"></i> </span>
-							</div>
-							<div class="form-group form-actions">
-								<span class="input-icon">
-									<input type="password" class="form-control password" name="password" placeholder="Password"><i class="fa fa-lock"></i>
-									 </span>
-							</div>
+
+<div class="form-group">
+<span class="input-icon">
+<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+<i class="fa fa-lock"></i> </span>
+</div>
+	
+
+<div class="form-group">
+<span class="input-icon">
+<input type="password" class="form-control"  id="password_again" name="password_again" placeholder="Password Again" required>
+<i class="fa fa-lock"></i> </span>
+</div>
+							
+
 							<div class="form-actions">
 								
-								<button type="submit" class="btn btn-primary pull-right" name="submit">
-									Login <i class="fa fa-arrow-circle-right"></i>
+								<button type="submit" class="btn btn-primary pull-right" name="change">
+								Cambio <i class="fa fa-arrow-circle-right"></i>
 								</button>
 							</div>
-							
+							<div class="new-account">
+							¿Ya tienes una cuenta?
+								<a href="userlogin.php">
+								Acceso
+								</a>
+							</div>
 						</fieldset>
 					</form>
 
 					<div class="copyright">
-						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> CLM</span>. <span>Derechos Reservados Del Autor</span>
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> CLM</span>. <span>Reservados todos los derechos</span>
 					</div>
 			
 				</div>
